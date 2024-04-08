cb56473f59e3e53235ddecd076d46526bad4fc12
SebzSGC <sebastian.gomez78@correo.tdea.edu.co>
2024-04-08 01:38:00 -0500

    CHG typing models

--- Django/hospital/hospitalApp/models.py
+++ Django/hospital/hospitalApp/models.py
@@ -3,128 +3,138 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
-    id = models.BigIntegerField(primary_key=True)
-    fullName = models.CharField(max_length=255)
-    email = models.EmailField()
-    phoneNumber = models.CharField(max_length=20)
-    bornDate = models.DateField()
-    address = models.CharField(max_length=200)
-    rol = models.CharField(max_length=30)
-    userName = models.CharField(max_length=80)
-    password = models.CharField(max_length=128)
+    id: int = models.BigIntegerField(primary_key=True)
+    fullName: str = models.CharField(max_length=255)
+    email: str = models.EmailField()
+    phoneNumber: str = models.CharField(max_length=20)
+    bornDate: str = models.DateField()
+    address: str = models.CharField(max_length=200)
+    rol: str = models.CharField(max_length=30)
+    userName: str = models.CharField(max_length=80)
+    password: str = models.CharField(max_length=128)
 
 
 class Medication(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    price = models.FloatField()
-    dosage = models.FloatField()
+    id: int = models.AutoField(primary_key=True)
+    name: str = models.CharField(max_length=255)
+    price: float = models.FloatField()
+    dosage: float = models.FloatField()
 
 
 class Procedure(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    id: int = models.AutoField(primary_key=True)
+    name: str = models.CharField(max_length=255)
+    description: str = models.CharField(max_length=255)
 
 
 class Patient(models.Model):
-    idDocument = models.BigIntegerField(primary_key=True)
-    fullName = models.CharField(max_length=255)
-    bornDate = models.DateField()
-    genre = models.CharField(max_length=50)
-    address = models.CharField(max_length=255)
-    phoneNumber = models.CharField(max_length=20)
-    email = models.EmailField()
+    idDocument: int = models.BigIntegerField(primary_key=True)
+    fullName: str = models.CharField(max_length=255)
+    bornDate: str = models.DateField()
+    genre: str = models.CharField(max_length=50)
+    address: str = models.CharField(max_length=255)
+    phoneNumber: str = models.CharField(max_length=20)
+    email: str = models.EmailField()
 
 
 class EmergencyContact(models.Model):
-    id = models.BigIntegerField(primary_key=True)
-    idPatient = models.ForeignKey(Patient, on_delete=models.CASCADE, to_field='idDocument', db_column='idPatient')
-    fullName = models.CharField(max_length=255)
-    relationship = models.CharField(max_length=20)
-    phoneNumber = models.CharField(max_length=20)
+    id: int = models.BigIntegerField(primary_key=True)
+    idPatient: int = models.ForeignKey(Patient, on_delete=models.CASCADE, to_field='idDocument',
+                                       db_column='idPatient')
+    fullName: str = models.CharField(max_length=255)
+    relationship: str = models.CharField(max_length=20)
+    phoneNumber: str = models.CharField(max_length=20)
 
 
 class MedicalInsurance(models.Model):
-    id = models.AutoField(primary_key=True)
-    idPatient = models.ForeignKey(Patient, on_delete=models.CASCADE, to_field='idDocument', db_column='idPatient')
-    nameOfInsuranceCompany = models.CharField(max_length=255)
-    policyNumber = models.BigIntegerField()
-    policyState = models.BooleanField()
-    policyValidity = models.DateField()
+    id: int = models.AutoField(primary_key=True)
+    idPatient: int = models.ForeignKey(Patient, on_delete=models.CASCADE, to_field='idDocument',
+                                       db_column='idPatient')
+    nameOfInsuranceCompany: str = models.CharField(max_length=255)
+    policyNumber: int = models.BigIntegerField()
+    policyState: bool = models.BooleanField()
+    policyValidity: str = models.DateField()
 
 
 class Order(models.Model):
-    id = models.AutoField(primary_key=True)
-    idPatient = models.ForeignKey(Patient, on_delete=models.CASCADE, to_field='idDocument', db_column='idPatient')
-    idDoctor = models.ForeignKey(Employee, on_delete=models.CASCADE, to_field='id', db_column='idDoctor')
-    creationDate = models.DateField()
+    id: int = models.AutoField(primary_key=True)
+    idPatient: int = models.ForeignKey(Patient, on_delete=models.CASCADE, to_field='idDocument',
+                                       db_column='idPatient')
+    idDoctor: int = models.ForeignKey(Employee, on_delete=models.CASCADE, to_field='id', db_column='idDoctor')
+    creationDate: str = models.DateField()
 
 
 class ClinicalHistory(models.Model):
-    id = models.BigIntegerField(primary_key=True)
-    idPatient = models.ForeignKey(Patient, on_delete=models.CASCADE, to_field='idDocument', db_column='idPatient')
-    idDoctor = models.ForeignKey(Employee, on_delete=models.CASCADE, to_field='id', db_column='idDoctor')
-    idOrder = models.ForeignKey(Order, on_delete=models.CASCADE, to_field='id', db_column='idOrder')
-    consultReason = models.CharField(max_length=255)
-    symptomatology = models.CharField(max_length=255)
-    diagnosis = models.CharField(max_length=255)
-    date = models.DateField()
+    id: int = models.BigIntegerField(primary_key=True)
+    idPatient: Patient = models.ForeignKey(Patient, on_delete=models.CASCADE, to_field='idDocument',
+                                           db_column='idPatient')
+    idDoctor: Employee = models.ForeignKey(Employee, on_delete=models.CASCADE, to_field='id', db_column='idDoctor')
+    idOrder: Order = models.ForeignKey(Order, on_delete=models.CASCADE, to_field='id', db_column='idOrder')
+    consultReason: str = models.CharField(max_length=255)
+    symptomatology: str = models.CharField(max_length=255)
+    diagnosis: str = models.CharField(max_length=255)
+    date: str = models.DateField()
 
 
 class VitalData(models.Model):
-    id = models.AutoField(primary_key=True)
-    idPatient = models.ForeignKey(Patient, on_delete=models.CASCADE, to_field='idDocument', db_column='idPatient')
-    arterialPressure = models.FloatField()
-    temperature = models.FloatField()
-    pulse = models.IntegerField()
-    bloodOxygenLevel = models.FloatField()
+    id: int = models.AutoField(primary_key=True)
+    idPatient: int = models.ForeignKey(Patient, on_delete=models.CASCADE, to_field='idDocument',
+                                       db_column='idPatient')
+    arterialPressure: float = models.FloatField()
+    temperature: float = models.FloatField()
+    pulse: int = models.IntegerField()
+    bloodOxygenLevel: float = models.FloatField()
 
 
 class MedicalAppointment(models.Model):
-    id = models.AutoField(primary_key=True)
-    idPatient = models.ForeignKey(Patient, on_delete=models.CASCADE, to_field='idDocument', db_column='idPatient')
-    idDoctor = models.ForeignKey(Employee, on_delete=models.CASCADE, to_field='id', db_column='idDoctor')
-    date = models.DateField()
+    id: int = models.AutoField(primary_key=True)
+    idPatient: int = models.ForeignKey(Patient, on_delete=models.CASCADE, to_field='idDocument',
+                                       db_column='idPatient')
+    idDoctor: int = models.ForeignKey(Employee, on_delete=models.CASCADE, to_field='id', db_column='idDoctor')
+    date: str = models.DateField()
 
 
 class Invoice(models.Model):
-    id = models.AutoField(primary_key=True)
-    idMedicalAppointment = models.ForeignKey(MedicalAppointment, on_delete=models.CASCADE, to_field='id', db_column='idMedicalAppointment')
+    id: int = models.AutoField(primary_key=True)
+    idMedicalAppointment: int = models.ForeignKey(MedicalAppointment, on_delete=models.CASCADE,
+                                                  to_field='id', db_column='idMedicalAppointment')
 
 
 class OrderMedication(models.Model):
-    id = models.AutoField(primary_key=True)
-    idOrder = models.ForeignKey(Order, on_delete=models.CASCADE, to_field='id', db_column='idOrder')
-    idMedication = models.ForeignKey(Medication, on_delete=models.CASCADE, to_field='id',   db_column='idMedication')
-    dose = models.FloatField()
-    treatmentDuration = models.CharField(max_length=100)
-    amount = models.FloatField()
-    item = models.IntegerField()
+    id: int = models.AutoField(primary_key=True)
+    idOrder: int = models.ForeignKey(Order, on_delete=models.CASCADE, to_field='id', db_column='idOrder')
+    idMedication: int = models.ForeignKey(Medication, on_delete=models.CASCADE, to_field='id',
+                                          db_column='idMedication')
+    dose: float = models.FloatField()
+    treatmentDuration: str = models.CharField(max_length=100)
+    amount: float = models.FloatField()
+    item: int = models.IntegerField()
 
 
 class OrderProcedure(models.Model):
-    id = models.AutoField(primary_key=True)
-    idOrder = models.ForeignKey(Order, on_delete=models.CASCADE, to_field='id', db_column='idOrder')
-    idProcedure = models.ForeignKey(Procedure, on_delete=models.CASCADE, to_field='id', db_column='idProcedure')
-    amount = models.FloatField()
-    frequency = models.CharField(max_length=100)
-    item = models.IntegerField()
+    id: int = models.AutoField(primary_key=True)
+    idOrder: int = models.ForeignKey(Order, on_delete=models.CASCADE, to_field='id', db_column='idOrder')
+    idProcedure: int = models.ForeignKey(Procedure, on_delete=models.CASCADE, to_field='id',
+                                         db_column='idProcedure')
+    amount: float = models.FloatField()
+    frequency: str = models.CharField(max_length=100)
+    item: int = models.IntegerField()
 
 
 class OrderProcedureAssistance(models.Model):
-    id = models.AutoField(primary_key=True)
-    idOrderProcedure = models.ForeignKey(OrderProcedure, on_delete=models.CASCADE, to_field='id', db_column='idOrderProcedure')
-    idSpecialist = models.CharField(max_length=255)
+    id: int = models.AutoField(primary_key=True)
+    idOrderProcedure: int = models.ForeignKey(OrderProcedure, on_delete=models.CASCADE, to_field='id',
+                                              db_column='idOrderProcedure')
+    idSpecialist: str = models.CharField(max_length=255)
 
 
 class OrderHelpDiagnostic(models.Model):
-    id = models.AutoField(primary_key=True)
-    idOrder = models.ForeignKey(Order, on_delete=models.CASCADE, to_field='id', db_column='idOrder')
-    nameHelpDiagnostic = models.CharField(max_length=255)
-    quantity = models.FloatField()
-    amount = models.FloatField()
-    item = models.IntegerField()
+    id: int = models.AutoField(primary_key=True)
+    idOrder: int = models.ForeignKey(Order, on_delete=models.CASCADE, to_field='id', db_column='idOrder')
+    nameHelpDiagnostic: str = models.CharField(max_length=255)
+    quantity: float = models.FloatField()
+    amount: float = models.FloatField()
+    item: int = models.IntegerField()
 
 # Create your mongomodels here.
 
